175555eed4edb3f94c515626f7166666fe4f938e
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-06-12 17:39:52 -0700

    Build server according to Bloc assignment

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [{
+    id: 'f183e068-54cb-43a0-8b46-8b7e13d35326',
+    url: 'https://www.facebook.com/deathmatchinhell/',
+    title: 'Deathmatch in Hell',
+    rating: 5,
+    description: '新宿ゴールデン街三番街の映画BAR地獄のデスマッチ。PM8-AM3やってます。日祝定休。 Deathmatch In Hell,we are. Open8pm-3am. Sunday,holiday closed.',
+}]
+
+module.exports = { bookmarks }
